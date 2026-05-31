a71c51960af43ed5a1055e35b892d4115acfdd2c
Grant Gryczan <grantgryczan@gmail.com>
2026-05-31 02:54:37 -0400

    Initial commit

---
+++ App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Parent from "./Parent.vue";
+</script>
+
+<template>
+  <Parent optional="override" />
+  <Parent required="override" />
+</template>
---
+++ Child.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+defineProps<{
+  required: string;
+  optional?: string;
+}>();
+</script>
